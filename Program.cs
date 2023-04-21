3664008d0db3668782e63b31b448d70e73bf31e0
main <eperry910@yahoo.com>
2023-04-21 10:05:46 -0400

    Coding Challenge

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class capitalization
+{
+    public static List<int> capitals(string input)
+    {
+        List<int> capitalLetters = new List<int>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Char.IsUpper(input[i]))
+            {
+                capitalLetters.Add(i);
+            }
+        }
+        return capitalLetters;
+    }
+    public static void Main(string[] args)
+    {
+        string testing = "NewAttemptFor!MThis";
+        List<int> test = capitals(testing);
+        foreach (int i in test)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
